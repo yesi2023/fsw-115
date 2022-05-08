7352068f6de72e620c1be70e5fc3c5139d637216
yesi2023 <irma.y.rodriguez@bryanuniversity.edu>
2022-05-08 14:45:15 -0500

    Axios To do 1

---
+++ week4/axios1.js
@@ -0,0 +1,27 @@
+axios.get("http://api.bryanuniversity.edu/irmarodriguez22/list/")
+    .then(result => {
+        result.data.forEach(function (x, i) {
+            let para = document.createElement("ol")
+            document.body.appendChild(para)
+
+            let description = document.createElement("li")
+            document.body.appendChild(description)
+
+            para.textContent = result.data[i].name
+            description.textContent = result.data[i].description
+
+
+            para.addEventListener('click', function () {
+                para.style.textDecoration = "line-through"
+            })
+
+            description.addEventListener('click', function () {
+                description.style.textDecoration = "line-through"
+            })
+           
+        })
+    })
+    .catch(err => console.log(err))
+
+
+
