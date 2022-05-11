65734942dad72be0e2ba9837029be27b56ed6834
yesi2023 <irma.y.rodriguez@bryanuniversity.edu>
2022-05-11 02:32:44 -0500

    axios

---
+++ week4/axiosProject2/pt2.js
@@ -0,0 +1,27 @@
+document
+  .getElementById("button")
+  .addEventListener("click", function () {
+    axios
+      .get("http://api.bryanuniversity.edu/IrmaRodriguez1/list/")
+      .then((result) => {
+        result.data.forEach(function (e, i) {
+          let para = document.createElement("ol");
+          document.body.appendChild(para);
+
+          let description = document.createElement("li");
+          document.body.appendChild(description);
+
+          para.textContent = result.data[i].name;
+          description.textContent = result.data[i].description;
+
+          para.addEventListener("click", function () {
+            para.style.textDecoration = "line-through";
+          });
+
+          description.addEventListener("click", function () {
+            description.style.textDecoration = "line-through";
+          });
+        });
+      });
+  })
+  .catch((err) => console.log(err));
